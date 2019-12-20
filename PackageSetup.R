428647a453e96a32ca6d8386cd1b960975301ff9
apeterson91 <adampeterso@gmail.com>
2019-12-20 16:12:30 -0500

    First commit

---
+++ PackageSetup.R
@@ -0,0 +1,15 @@
+
+# Install Libraries -------------------------------------------------------
+
+install.packages("Rcpp")
+
+
+# Package Skeleton --------------------------------------------------------
+
+Rcpp::Rcpp.package.skeleton(name = "dragonsfirstRcpp")
+
+
+
+
+
+
